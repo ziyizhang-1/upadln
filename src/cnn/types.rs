a7ee75944992fce720ba7c5a8163ee7528fd273d
ziyizhang-1 <ziyi.zhang@intel.com>
2024-03-26 19:49:46 -0700

    add tests

--- src/cnn/types.rs
+++ src/cnn/types.rs
@@ -3,8 +3,8 @@ use openvino::{Core, Blob, TensorDesc};
 use std::time::Instant;
 use crate::utils::openvino_tensor_converter::{convert, Dimensions, Precision};
 
-static MODEL_XML_PATH: &'static str = "/home/ziyi/tmp/yolov8n_openvino_model_fp16/yolov8n.xml";
-static MODEL_WEIGHTS_PATH: &'static str = "/home/ziyi/tmp/yolov8n_openvino_model_fp16/yolov8n.bin";
+static MODEL_XML_PATH: &'static str = "./tests/yolov8n_openvino_model_fp16/yolov8n.xml";
+static MODEL_WEIGHTS_PATH: &'static str = "./tests/yolov8n_openvino_model_fp16/yolov8n.bin";
 
 type Error = Box<dyn std::error::Error + 'static>;
 
@@ -15,20 +15,19 @@ pub fn run() -> Result<(), Error> {
     let mut model = core.load_network(&cnn, "CPU")?;
     let mut irq = model.create_infer_request()?;
     
-    let tensors = convert("/home/ziyi/tmp/bus.jpg", &Dimensions::new(640, 640, 3, Precision::FP32))?;
+    let tensors = convert("./tests/images/bus.jpg", &Dimensions::new(640, 640, 3, Precision::FP32))?;
     let input = Blob::new(&input_shape, &tensors)?;
     // irq.set_batch_size(1)?;
 
     irq.set_blob("x", &input).unwrap();
-    // irq.set_blob("Result_3426", &output).unwrap();
 
     let now = Instant::now();
     (0..1000).into_iter().for_each(|_| {
         irq.infer().unwrap();
     });
-    // irq.infer().unwrap();
     let output = irq.get_blob("__module.model.22/aten::cat/Concat_5").unwrap();
     // println!("{:#?}", unsafe { output.buffer_as_type::<f32>().unwrap() });
+    println!("{:#?}", output.tensor_desc()?.dims());
     println!("{:#?}", now.elapsed());
 
     Ok(())
